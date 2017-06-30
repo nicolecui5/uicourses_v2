e76d3eac3b39a9553b3d1874a7b97ba6ebb1a561
UICourses <server@uicourses.com>
2017-06-29 21:58:27 -0700

    updated docs

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# uicourses_v2
+# UICourses v2
+
+## Database -> JSON API
+### Scripts
+- `server.py`: a simple TCP server that acts as an API that pulls data from the database and parse them into json format.
+- `dbutil.py`: database lookup functions used by `server.py`
+- `options.py`: database options used in `dbutil.py`. Mostly hardcoded column names.
+- `util.py`: general utilities (logging, etc)
+- `credentials.py`: not to be committed. The format is shown in `credentials.py.format` - on has to rename it to `credentials.py` and fill in the credential strings.
+
+### Usage
+To start the server, run
+```
+./server.py <port>
+```
+
+Just shoot a GET request like this:
+```
+http://<domain>:<port>/dbapi?subject=<subject>&code=<course_code>&[suffix=<suffix>]&key=<api_key>
+```
+such as
+```
+http://uicourses.com:7000/dbapi?subject=cs&code=374&key=mykey
+```
---
+++ credentials.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+# UICourses Project
+#
+# Authored by:   WSB & Suyie
+# Drafted:       June 28, 2017
+# Last modifed:  June 28, 2017
+# Description:   This script contains hardcoded credentials
+#                DO NOT COMMIT TO PUBLIC REPOs!!!
+#                DO NOT COMMIT TO PUBLIC REPOs!!!
+#                DO NOT COMMIT TO PUBLIC REPOs!!!
+#
+###############################################################################
+
+
+###############################################################################
+# DATABASE CREDENTIALS
+###############################################################################
+
+DB = {
+    'username': 'uicourses',
+    'password': 'Haochideyumi!',
+    'database': 'uicourse_uicourses_v2'
+}
+
+
+###############################################################################
+# API KEYS
+###############################################################################
+
+KEYS = [
+    'mykey',
+]
