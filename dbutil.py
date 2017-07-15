1ef40f249c6a4d74f80488bdef2953e50da6caf4
Suyie <qilezhi2@illinois.edu>
2017-07-15 15:09:50 -0500

    pie chart added

--- dbutil.py
+++ dbutil.py
@@ -314,7 +314,7 @@ def lookup_course(db, subject, code, suffix=''):
     '''
     
     res['Pie_Script'] = pie_js1 + pie_js2.format(res['Pct_Lecture'], res['Pct_Discussion'], res['Pct_Homework'], res['Pct_Lab'], res['Pct_Quiz'], res['Pct_Midterm'], res['Pct_Project'], res['Pct_Final'], res['Pct_ExtraCredit'], res['Pct_Other']) + pie_js3
-    res['Pie_Script'] = res['Pie_Script'].replace('\n', '').replace('  ', ' ')
+    res['Pie_Script'] = res['Pie_Script'].replace('\n', '').replace('  ', ' ').replace(unicode('&quot;'), '"')
 
     return res
 
